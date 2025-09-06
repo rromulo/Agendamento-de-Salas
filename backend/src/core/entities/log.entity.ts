57780a7fd025daeb4a32a33d856f1888d1946fa7
rromulo <rromulo.silva21@gmail.com>
2025-09-05 21:06:55 -0300

    chore: create log entity

---
+++ backend/src/core/entities/log.entity.ts
@@ -0,0 +1,35 @@
+export type TLogAction = 
+  | "LOGIN"
+  | "PAGE_ACCESS"
+  | "DATA_CREATE"
+  | "DATA_EDIT"
+  | "DATA_VIEW"
+  | "DATA_DELETE"
+  | "LOGOUT";
+
+export class Log {
+  public id: number | null;
+  public userId: string;
+  public action: TLogAction;
+  public description: string;
+  public timestamp: Date;
+
+  constructor(
+    id: number | null,
+    userId: string,
+    action: TLogAction,
+    description: string
+  ) {
+    this.id = id;
+    this.userId = userId;
+    this.action = action;
+    this.description = description;
+    this.timestamp = new Date();
+  }
+
+  public toString(): string {
+    const logMessage = `[${this.timestamp.toISOString()}] User ${this.userId} performed ${this.action}: ${this.description}`
+    return logMessage
+  }
+
+}

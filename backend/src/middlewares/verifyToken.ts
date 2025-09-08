18d6f3d549b7bc778d4eee7f35b1364f1860a979
rromulo <rromulo.silva21@gmail.com>
2025-09-08 02:35:16 -0300

    feat/build: Feat - create verify token. Build - install joi

---
+++ backend/src/middlewares/verifyToken.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import ApiError from '@utils/apiError';
+
+export default interface IJWTPayload {
+  id: string;
+  email: string
+  name: string;
+  role: string;
+}
+
+export default class JsonWebToken {
+  private _secret: jwt.Secret;
+  private _payload: IJWTPayload | undefined;
+  private _authorization: string | undefined;
+
+  constructor(payload?: IJWTPayload) {
+    this._payload = payload;
+    this._secret = process.env.JWT_SECRET || 'jwt_secret';
+  }
+
+  public verifyToken(authorization: string): IJWTPayload {
+    if(!authorization) throw new ApiError(401, 'Token not found.');
+    try {
+      this._authorization = authorization
+      
+      const decoded = jwt.verify(this._authorization, this._secret) as IJWTPayload;
+      return decoded
+    } catch (error) {
+      throw new ApiError(401, 'Token invalid.');
+    }
+  }
+}
